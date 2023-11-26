9449f1cc7c88d6f24b27f6ae64bd3487b50aeca3
Andrés Aristizábal <andres.aristizabal@gmail.com>
2023-11-26 11:24:00 -0500

    feat: implementación de log en servicio de email

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -1,5 +1,7 @@
 import nodemailer from 'nodemailer'
 import { envs } from '../../config/plugins/envs.plugin'
+import { LogRepository } from '../../domain/repository/log.repository'
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity'
 
 interface SendMailOptions {
     to: string | string[],
@@ -22,6 +24,10 @@ export class EmailService {
         }
     })
 
+    constructor(private readonly logRepository: LogRepository) {
+
+    }
+
     async sendEmail(options: SendMailOptions): Promise<boolean> {
 
         const { to, subject, htmlBody, attachments = [] } = options
@@ -35,11 +41,22 @@ export class EmailService {
                 attachments
             })
 
-            console.log(sentInformation)
+            // console.log(sentInformation)
+            const log = new LogEntity({
+                level: LogSeverityLevel.low,
+                message: 'Email sent',
+                origin: 'email.service.ts'
+            })
+            this.logRepository.saveLog(log)
 
             return true
         } catch (error) {
-
+            const log = new LogEntity({
+                level: LogSeverityLevel.high,
+                message: 'Email not sent',
+                origin: 'email.service.ts'
+            })
+            this.logRepository.saveLog(log)
             return false
         }
     }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -11,32 +11,24 @@ export class Server {
     public static start() {
         console.log('Server started...')
 
-        const emailService = new EmailService()
-        // emailService.sendEmail({
-        //     to: 'andres.aristizabal@gmail.com',
-        //     subject: 'Logs de sistem',
-        //     htmlBody: `
-        //     <h3>Logs de Sistema - NOC</h3>
-        //     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a dictum mi, ut accumsan justo</p>
-        //     <p>Ver logs adjuntos</p>
-        //     `
-        // })
+        const emailService = new EmailService(fileSystemLogRepository)
+
         emailService.sendEmailWithFileSystemLogs(
             ['andres.aristizabal@gmail.com', 'danyramirezm93@hotmail.com']
         )
 
-        // CronService.createJob(
-        //     '*/5 * * * * *',
-        //     () => {
-        //         const url: string = 'https://google.com'
-        //         // const url: string = 'http://localhost:3000'
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url: string = 'https://google.com'
+                // const url: string = 'http://localhost:3000'
 
-        //         new CheckService(
-        //             fileSystemLogRepository,
-        //             () => console.log(`${url} is ok`),
-        //             (error) => console.log(error)
-        //         ).execute(url)
-        //     }
-        // )
+                new CheckService(
+                    fileSystemLogRepository,
+                    () => console.log(`${url} is ok`),
+                    (error) => console.log(error)
+                ).execute(url)
+            }
+        )
     }
 }
